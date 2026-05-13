f32c08181ba3f6626900a6ebada635bfee0cdad2
Austin-Hub-code00 <austinhuang2005@gmail.com>
2026-05-13 19:20:41 +0800

    fix

--- admin.js
+++ admin.js
@@ -1,27 +1,43 @@
 let adminDiv =
 document.getElementById("admin-orders");
+
 window.onSnapshot(
 window.collection(window.db, "orders"),
 (snapshot) => {
+
 adminDiv.innerHTML = "";
+
 snapshot.forEach((docItem) => {
+
 let order = docItem.data();
+
 adminDiv.innerHTML +=
 `
- <div class="admin-order">
- <h3>Order #${order.number}</h3>
- <h3>${order.customer}</h3>
- <p>${order.foods}</p>
- <p>Total: $${order.total}</p>
- <button onclick="completeOrder('${docItem.id}')">
- Complete
- </button>
- </div>
- `;
+<div class="admin-order">
+
+<h3>Order #${order.number}</h3>
+
+<h3>${order.customer}</h3>
+
+<p>${order.foods}</p>
+
+<p>Total: $${order.total}</p>
+
+<button onclick="completeOrder('${docItem.id}')">
+Complete
+</button>
+
+</div>
+`;
+
 });
+
 });
+
 window.completeOrder = async function(id) {
+
 await window.deleteDoc(
 window.doc(window.db, "orders", id)
 );
+
 }
